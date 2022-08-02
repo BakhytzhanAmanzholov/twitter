6a14c49665270dd8a5c2c214e5a587670f9ee555
BakhytzhanAmanzholov <amanzholovbakhytzhan@gmail.com>
2022-08-02 16:18:28 +0600

    Adding docker-compose

---
+++ Twitter/src/main/java/kz/akvelon/twitter/TwitterApplication.java
@@ -0,0 +1,28 @@
+package kz.akvelon.twitter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@SpringBootApplication(scanBasePackages = {"kz.akvelon.twitter"})
+public class TwitterApplication {
+
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterApplication.class, args);
+    }
+
+
+}
